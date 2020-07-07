abc80d73c0da6b0194d4e3ec4dc909fac842c4d8
Bruce Hansen <60013584+golfhans@users.noreply.github.com>
2020-07-07 15:16:42 -0500

    Add files via upload

---
+++ excel_to_csv.ps1
@@ -0,0 +1,14 @@
+﻿Function ExcelToCsv ($File) {
+    $myDir = "D:\MyDocs\CurrentWorkDocuments\20200601_Summit_Files"
+    $excelFile = "$myDir\" + "$File" + ".xlsx"
+    $Excel = New-Object -ComObject Excel.Application
+    $wb = $Excel.Workbooks.Open($excelFile)
+	
+    foreach ($ws in $wb.Worksheets) {
+        $ws.SaveAs("$myDir\" + "$File" + "_" + $ws.name  + ".csv", 6)
+    }
+    $Excel.Quit()
+}
+
+$FileName = "MARS_HEFF_export_CF_04.30.2020"
+ExcelToCsv -File $FileName
